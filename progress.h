159104a11de38dba25b93aa7ed0ff776f2e79b0b
Georg-Johann Lay <avr@gjlay.de>
2026-02-09 11:51:31 +0100

    Progress += done(), restart().

--- progress.h
+++ progress.h
@@ -2,8 +2,9 @@
 #ifndef PROGRESS_H
 #define PROGRESS_H
 
-#include <cstdio>
 #include <string>
+#include <cstdio>
+#include <cassert>
 
 typedef struct {} progress_with_total_t;
 static inline constexpr progress_with_total_t PROGRESS_WITH_TOTAL {};
@@ -17,17 +18,24 @@ struct Progress
     T margin;
     T last_report = 0;
     bool started = false;
+    bool done_p = false;
     // 1st % refers to the current progress in T.
     // 2nd % refers to the prgress in percent relative to total.
     std::string fmt;
     Progress (progress_with_total_t, T total, T margin, const char *fmt)
         : total(total), margin(margin), fmt(fmt)
     {
-        update ((T) 0);
+        restart ();
     }
     Progress (const Progress<T>&) = delete;
+    void restart ()
+    {
+        started = done_p = 0;
+        update ((T) 0);
+    }
     void update (T t)
     {
+        assert (! done_p && "done't update a done Progress");
         if (! started || t - last_report >= margin)
         {
             printf (back);
@@ -37,11 +45,19 @@ struct Progress
         started = true;
         last_report = t;
     }
+    void done ()
+    {
+        if (! done_p)
+        {
+            printf ("%s                                    %s%s",
+                    back, back, back);
+            fflush (stdout);
+        }
+        done_p = true;
+    }
     ~Progress ()
     {
-        printf ("%s                                    %s%s",
-                back, back, back);
-        fflush (stdout);
+        done ();
     }
 };
 #endif // PROGRESS_H
